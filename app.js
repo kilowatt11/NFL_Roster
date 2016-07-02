e2bf4544255591ff94319500d37eeb48769a3fc8
Kurt Hammes <kilowatt11@cableone.net>
2016-07-01 20:51:05 -0600

    form submit now working. players displayed to DOM

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+function Player(playername,position,jersey){
+    var newPlayer = this
+
+  newPlayer.playername = playername;
+  newPlayer.position = position;
+  newPlayer.jersey = jersey;
+  newPlayer.img = "http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"
+
+}
+
+var players = []
+
+
+$('form').on('submit', function(e){
+    // debugger;
+  e.preventDefault();
+  var form = this;
+  var player = new Player(form.playerName.value, form.position.value,form.jersey.value)
+  
+  players.push(player)
+  
+  update(players)
+  console.log(players)
+  form.reset();
+})
+
+
+function update(list){
+
+  var playerElem = $('.player-roster');
+  playerElem.empty()
+  for(var i = 0; i < list.length; i++){
+      console.log(list[i])
+    playerElem.append('<div class="player-card text-center"><img class="media-object img-rounded" src="' + list[i].img + '"alt ="..."/><p>'+ list[i].playername +'</p><p>'+list[i].position +'</p><p>'+list[i].jersey+'</p></div>')  
+  }
+}
+
+$('.player-roster').on('click', '.player-card', function(){
+  var username = $(this).text()
+  removeUserByName(playername);
+  update(players);
+})
+ 
+function removeUserByName(name){
+   for (var i = 0; i < players.length; i++) {
+     var currentPlayer = players[i];
+     if(currentPlayer.name == name){
+       return players.splice(i,1)
+     }
+   }
+}
