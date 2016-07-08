6ee675a7b39814bf432156379d77520e307a4607
Kurt Hammes <kilowatt11@cableone.net>
2016-07-08 16:31:48 -0600

    added save roster to local storage button

--- nfl-roster-controller.js
+++ nfl-roster-controller.js
@@ -68,12 +68,23 @@ function ready(dataStore) {
     function draftUpdate() {
         
         draftRoster.empty();
+        
+
+
         myPlayers.forEach(function (player) {
+            console.log(myPlayers)
             var template = '<div id="player-card" class=" text-center"><img class="media-object img-rounded" src="' + player.photo + '"alt ="..."/><p id="player-name">' + player.fullname + '</p><p>' + player.position + '</p><p>' + player.jersey + '</p></div>'
             draftRoster.append(template);
         })
     }
     /*END REMOVE PLAYER FROM ROSTER*/
+
+$('.saveRoster').on('click', function () {
+    
+         localStorage.setItem('myTeam', JSON.stringify(myPlayers))
+         draftUpdate();
+
+    })
 
     /*FILTER NFL PLAYER LIST*/
     $('#find').on('click', function (event) {
@@ -91,9 +102,18 @@ function ready(dataStore) {
 
     function update() {
         roster.empty()
+        
+        var data = localStorage.getItem('myTeam');
+        if(data){
+        playerData = JSON.parse(data)
+        myPlayers = playerData
+        draftUpdate();
+
+        }
 
         roster.append('<h3>Showing ' + players.length + ' Active Players</h3>')
         players.forEach(function (player) {
+            console.log(players)
             var template = '<ul class="player-list"><li id="draft-pick"><img src="' + player.photo + '" width="80"/>' + player.fullname + '</li></ul>'
             roster.append(template);
         })
