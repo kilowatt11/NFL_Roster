33d5ec8418147676bdec927ec34d5cacac859e37
Kurt Hammes <kilowatt11@cableone.net>
2016-07-07 23:45:01 -0600

    add/remove works. Check fnctn restricts duplicates

--- nfl-roster-controller.js
+++ nfl-roster-controller.js
@@ -9,15 +9,22 @@ function ready(dataStore) {
 
     var myPlayers = [];
 
-    /*add to draft on click event*/
+    /*START DRAFT NEW PLAYER ON CLICK*/
     $('.roster').on('click', '#draft-pick', function () {
-        // debugger;
+        debugger;
         var username = $(this).text()
-        addUserByName(username);
+        var check = checkPlayer(username);
+
+
+        addUserByName(username, check);
         draftUpdate();
     })
-    function addUserByName(name) {
+    function addUserByName(name, check) {
         debugger;
+        if (check == true) {
+            return;
+        }
+
         for (var i = 0; i < players.length; i++) {
             var currentPlayer = players[i];
             if (currentPlayer.fullname == name) {
@@ -27,22 +34,55 @@ function ready(dataStore) {
         }
     }
 
-    function draftUpdate() {
+    function checkPlayer(name) {
         debugger;
+        for (var i = 0; i < myPlayers.length; i++) {
+            var currentPlayer = myPlayers[i];
+            if (currentPlayer.fullname == name) {
+                return true;
+            }
+        }
+    }
+    /*END DRAFT NEW PLAYER*/
+
+
+    /*START REMOVE PLAYER FROM ROSTER*/
+    $('.draft-roster').on('click', '#player-name', function () {
+        // debugger;
+        var username = $(this).text()
+        removeUserByName(username);
+        draftUpdate();
+    })
+
+
+    function removeUserByName(name) {
+        // debugger;
+        for (var i = 0; i < myPlayers.length; i++) {
+            var currentPlayer = myPlayers[i];
+            if (currentPlayer.fullname == name) {
+                myPlayers.splice(i, 1)
+                draftUpdate();
+            }
+        }
+    }
+
+    function draftUpdate() {
+        // debugger;
+        draftRoster.empty();
         myPlayers.forEach(function (player) {
-            var template = '<div class="player-card text-center"><img class="media-object img-rounded" src="' + player[i].img + '"alt ="..."/><p>' + player[i].playername + '</p><p>' + player[i].position + '</p><p>' + player[i].jersey + '</p></div>'
+            var template = '<div id="player-card" class=" text-center"><img class="media-object img-rounded" src="' + player.photo + '"alt ="..."/><p id="player-name">' + player.fullname + '</p><p>' + player.position + '</p><p>' + player.jersey + '</p></div>'
             draftRoster.append(template);
         })
     }
-    /*end draft*/
+    /*END REMOVE PLAYER FROM ROSTER*/
 
-
+    /*FILTER NFL PLAYER LIST*/
     $('#find').on('click', function (event) {
-        
         var query = $('#query').val()
         players = dataStore.getPlayersBySomeValue(query)
         update();
     })
+
 
     $('.clear-filter-button').on('click', function () {
         players = dataStore.getAllPlayers()
@@ -51,18 +91,15 @@ function ready(dataStore) {
 
 
     function update() {
-    
-
         roster.empty()
-        roster.append('<h3>Showing ' + players.length + ' Active Players</h3>')
 
+        roster.append('<h3>Showing ' + players.length + ' Active Players</h3>')
         players.forEach(function (player) {
             var template = '<ul class="player-list"><li id="draft-pick"><img src="' + player.photo + '" width="80"/>' + player.fullname + '</li></ul>'
             roster.append(template);
         })
-
     }
 
     update();
-
+    /*END FILTER NFL PLAYER LIST*/
 }
